b6f165aa5e075db121f9d67a7d28f1c80f3bf54e
Krishna Priya Nimmagadda <101842284+krishnapriya-n@users.noreply.github.com>
2022-10-14 14:33:55 +1300

    Update kleeMain.py

--- kleeMain.py
+++ kleeMain.py
@@ -30,7 +30,7 @@ class Klee:
     "such as 'who are you' or 'what are you' (ending -are you)"
 
     def introduce():
-        playsound ('C:/Users/lenovo/Desktop/Klee VA/Voices/Klee_Hello.mp3')
+        playsound ('C:/Users/lenovo/Desktop/Klee VA/Voices/Klee_Hello.mp3')         # saved on C drive on system (change as required)
 
     ## HELLO FUNCTION ##
     "Hello function is played when user prompts by saying statements"
@@ -38,50 +38,57 @@ class Klee:
     "randomly. We use the random module here."
 
     def hello():
-        path = 'C:/Users/lenovo/Desktop/Klee VA/Voices/Hellos/'
+        path = 'C:/Users/lenovo/Desktop/Klee VA/Voices/Hellos/'                     # saved on C drive on system (change as required)
         files = os.listdir(path)
         try:
             rand = random.choice(files)
-            playsound('C:/Users/lenovo/Desktop/Klee VA/Voices/Hellos/' + rand)
+            playsound('C:/Users/lenovo/Desktop/Klee VA/Voices/Hellos/' + rand)      # saved on C drive on system (change as required)
         
         except Exception as e:
-            print(e)
+            print(e) # error returned
 
     ## FRIEND FUNCTION ##
     "Friend function is played when user prompts by saying statements"
     "such as 'who is your friend' (contains 'friend' in statement)"
 
     def friend():
-        playsound ('C:/Users/lenovo/Desktop/Klee VA/Voices/Klee_Friend.mp3')
+        playsound ('C:/Users/lenovo/Desktop/Klee VA/Voices/Klee_Friend.mp3')        # saved on C drive on system (change as required)
 
     ## BIRTHDAY FUNCTION ##
     "Birthday function is played when user prompts by saying statements"
     "such as 'It's my birthday' (contains 'birthday' in statement)"
 
     def birthday():
-        playsound ('C:/Users/lenovo/Desktop/Klee VA/Voices/Klee_Birthday.mp3')
+        playsound ('C:/Users/lenovo/Desktop/Klee VA/Voices/Klee_Birthday.mp3')      # saved on C drive on system (change as required)
 
     ## HOBBIES FUNCTION ##
-    5
+    "Hobbies function is played when user prompts by saying statements"
+    "such as 'What are your hobbies?' (contains 'hobby'/ 'hobbies'/ 'like' in statement)"
 
     def hobbies():
-        playsound ('C:/Users/lenovo/Desktop/Klee VA/Voices/Klee_Hobbies.mp3')
+        playsound ('C:/Users/lenovo/Desktop/Klee VA/Voices/Klee_Hobbies.mp3')       # saved on C drive on system (change as required)
 
     ## ACTIVITIES FUNCTION ##
+    "Activited function is played when user prompts by saying statements"
+    "such as 'What did you do today?' (contains 'did you do' in statement)"
+    
     def today():
-        playsound ('C:/Users/lenovo/Desktop/Klee VA/Voices/Klee_Spoils.mp3')
+        playsound ('C:/Users/lenovo/Desktop/Klee VA/Voices/Klee_Spoils.mp3')        # saved on C drive on system (change as required)
 
     ## WISHING THE USER FUNCTION ##
+    "Activated when user prompts word 'good' in sentence"
+    "Good Morning/ Good Afternoon/ Good Evening/ Good Night"
+    
     def wishMe():
         hour = int(datetime.datetime.now().hour)
         if hour >= 0 and hour < 12:
-            playsound ('C:/Users/lenovo/Desktop/Klee VA/Voices/Klee_Morning.mp3')
+            playsound ('C:/Users/lenovo/Desktop/Klee VA/Voices/Klee_Morning.mp3')   # saved on C drive on system (change as required)
         elif hour >= 12 and hour < 15:
-            playsound ('C:/Users/lenovo/Desktop/Klee VA/Voices/Klee_Afternoon.mp3')
+            playsound ('C:/Users/lenovo/Desktop/Klee VA/Voices/Klee_Afternoon.mp3') # saved on C drive on system (change as required)
         elif hour >= 15 and hour < 19:
-            playsound ('C:/Users/lenovo/Desktop/Klee VA/Voices/Klee_Evening.mp3')
+            playsound ('C:/Users/lenovo/Desktop/Klee VA/Voices/Klee_Evening.mp3')   # saved on C drive on system (change as required)
         else:
-            playsound ('C:/Users/lenovo/Desktop/Klee VA/Voices/Klee_Night.mp3')
+            playsound ('C:/Users/lenovo/Desktop/Klee VA/Voices/Klee_Night.mp3')     # saved on C drive on system (change as required)
 
     ## INPUT FROM MICROPHONE ##   
     """It takes microphone input from the user and returns string output"""
@@ -92,7 +99,7 @@ class Klee:
             print("Listening...")
             r.pause_threshold = 1
             audio = r.listen(source)
-
+        # try/ except to catch errors
         try:
             print("Recognizing...")    
             query = r.recognize_google(audio, language='en-in')
@@ -122,7 +129,7 @@ if __name__ == "__main__":
         elif 'birthday' in query:
             Klee.birthday()
 
-        elif 'like' in query:
+        elif 'like' in query or 'hobby' in query or 'hobbies' in query:
             Klee.hobbies()
 
         elif 'did you do'in query:
